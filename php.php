2f4d26aaf0e7a0c501f00d6aa9a4f6c32bbd24fe
rafedkeles <115433653+rafedkeles@users.noreply.github.com>
2024-05-18 21:23:44 +0300

    Update php.php

--- php.php
+++ php.php
@@ -1,1 +1,18 @@
-
+<?php
+$d_kullanici_adi = 'g201210590@sakarya.edu.tr';
+$d_sifre = 'g201210590';
+if (isset($_POST['kullanici_adi']) && isset($_POST['sifre'])) {
+    $kullanici_adi = $_POST['kullanici_adi'];
+    $sifre = $_POST['sifre'];
+    if ($kullanici_adi == $d_kullanici_adi && $sifre == $d_sifre) {
+        echo "Hoşgeldiniz b1812100001";
+    } else {
+        echo "<script>alert('E-posta adresi veya şifre yanlış')</script>";
+        header("Location: giris.html");
+        exit; 
+    }
+} else {
+    echo "<script>alert('E-posta adresi veya şifre bulunmamaktadır')</script>";
+    header("Location: giris.html");
+}
+?>
